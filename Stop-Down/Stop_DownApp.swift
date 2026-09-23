25e4fc659da85cf880601946e8224c6ed8cd8f35
John Hoaglun <john@hoaglun.com>
2026-09-22 19:26:00 -0500

    Initial Commit

---
+++ Stop-Down/Stop_DownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_DownApp.swift
+//  Stop-Down
+//
+//  Created by John Hoaglun on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Stop_DownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
